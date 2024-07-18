2df1efa7cc67f2cdd1038134f130923e04fa2b23
Sumit <sumitbalawan@gmail.com>
2024-07-18 11:34:39 +0530

    first commit my

---
+++ S1.java
@@ -0,0 +1,18 @@
+public class S1{
+ public static void main(String[]args){
+    char ch='a';
+    
+    witch(ch)
+
+    {
+        case 'a':
+         case 'e':
+         case 'i': 
+         case 'o': 
+         case 'u': System.out.println("Vowels");
+         break;
+         default : System.out.println("consonent");
+
+    }
+ }
+}
